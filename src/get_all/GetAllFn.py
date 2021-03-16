21022eab78b14ceb627da845b5744784bfe4011d
lucas souza <ibmstemp@gmail.com>
2021-03-16 10:04:31 -0300

    feat: get all

--- src/get_all/GetAllFn.py
+++ src/get_all/GetAllFn.py
@@ -1,8 +1,26 @@
+import requests
+import boto3
+import os
+
+ENV = os.getenv("ENV") or 'dev'
+table_name = 'Bugs'
+ssm_client = boto3.client('ssm')
+
+API_KEY = ssm_client.get_parameter(
+    Name=f'/airtable/{ENV}/bug_api_key'
+)
+
+
+def helper_get_all():
+    response = requests.get(
+        url=f'https://api.airtable.com/v0/appyxraZ9ECUTtWRI/{table_name}?api_key={API_KEY}'
+    )
+    return response.json()
+
+
 class GetAll(event, context):
     def handler(self):
-        return {
-            "staus": 200
-        }
+        return helper_get_all()
 
 
 def handler(event, context):
